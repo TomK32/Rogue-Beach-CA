4bc7f6272dee7418168d9ca69e50ed6759a47bb8
Thomas R. Koll <tomk32@tomk32.de>
2013-01-11 23:54:53 +0100

    Prepare Player to draw in different states: standing and paddling

--- actors/player.lua
+++ actors/player.lua
@@ -31,20 +31,48 @@ function Player:initialize(position)
   self.dt_since_input = 0
   self.entity_type = 'Actor'
   self.inputs = {}
+  self.state = 'standing'
+  self:setBoard()
   self:setInputs(Player.input_alternatives['wasd'])
   self:setInputs(Player.input_alternatives['arrows'])
 end
 
 function Player:draw()
+  local board = {}
   game.renderer:print('@', {255,0,0,255}, self.position.x, self.position.y)
-  game.renderer:print('|', {255,100,0,255}, self.position.x + 1, self.position.y - 1)
-  game.renderer:print('|', {255,100,0,255}, self.position.x + 1, self.position.y)
-  game.renderer:print('|', {255,100,0,255}, self.position.x + 1, self.position.y + 1)
+  for i, tile in pairs(self.board) do
+    game.renderer:print(tile.c, {255,100,0,255}, self.position.x + tile.x, self.position.y - tile.y)
+  end
 end
+
 function Player:setInputs(inputs)
   for direction, key in pairs(inputs.keyboard) do
     self.inputs[key] = Player.movements[direction]
   end
 end
 
+function Player:switchState()
+  if self.state == 'standing' then
+    self.state = 'paddling'
+  elseif self.state == 'padding' then
+    self.state = 'surfing'
+  else
+    self.self = 'standing'
+  end
+  self:setBoard()
+end
 
+function Player:setBoard()
+  if self.state == 'standing' then
+    self.board = {
+      {x = 1, y = -1, c = '|'},
+      {x = 1, y = 0, c = '|'},
+      {x = 1, y = 1, c = '|'}
+    }
+  elseif self.state == 'paddling' then
+    self.board = {
+      {x = 0, y = -1, c = '▒'},
+      {x = 0, y = 1, c = '▒'}
+    }
+  end
+end
