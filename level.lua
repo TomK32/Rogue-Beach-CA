ddb5310b640e22b8c02613508d23b289b0dcfbe3
Thomas R. Koll <tomk32@tomk32.de>
2013-01-17 21:25:44 +0100

    Don't update Player twice in every tick

--- level.lua
+++ level.lua
@@ -19,7 +19,9 @@ function Level:update(dt)
   self.dt = self.dt + dt
   for layer, entities in pairs(self.map.layers) do
     for i, entity in pairs(entities) do
-      entity:update(dt)
+      if not entity.moved then
+        entity:update(dt)
+      end
       if entity.dead == true then
         --print("Removing", entity.position.y)
         table.remove(self.map.layers[layer], i)
