f14461d52860b30d7729bd2ef45ce2050256b345
Thomas R. Koll <tomk32@tomk32.de>
2013-01-30 23:40:38 +0100

    Set game name in config

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 require('version')
 function love.conf(c)
-  c.title = "Kollum: The Pressure Valve - " .. game.version
+  c.title = "Rogue Beach, CA v" .. game.version
   c.author = "Thomas R. Koll"
   c.screen.width = 1300
   c.screen.height = 700
